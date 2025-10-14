820138c3ae6fcae4f172c1a6efc55c703b45ce60
CUHP_VN <phuc260706@gmail.com>
2025-10-14 19:52:43 +0700

    hot fix
    :)))

--- Assets/_Game/_Scripts/Camera/CameraControler.cs
+++ Assets/_Game/_Scripts/Camera/CameraControler.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 
 public class CameraControler : MonoBehaviour
 {
+    [SerializeField] private Camera _camera;
     private CameraAdjust cameraAdjust = new CameraAdjust();
     public void UpdateCameraByGrid(Vector2 gridPos,Vector2 cellSize,Vector2 gridCenter,Vector2 gridSize)
     {
-       
-        Camera.main.transform.position = cameraAdjust.CalPos(gridPos,cellSize,gridCenter);
-        Camera.main.orthographicSize = cameraAdjust.CalOrthoSize(cellSize * gridSize);
+        if (_camera == null) _camera = GetComponent<Camera>();
+        _camera.transform.position = cameraAdjust.CalPos(gridPos,cellSize,gridCenter);
+        _camera.orthographicSize = cameraAdjust.CalOrthoSize(cellSize * gridSize);
     }
 }
